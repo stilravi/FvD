a0ff24eb8bd307f6e1e0337ed0d825f0057bc546
Ravi Stil <ravi.raaf@gmail.com>
2021-06-07 15:00:54 +0200

    Update

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,24 +1,46 @@
-// JavaScript Document
-const list = document.querySelector('.list');
-const URL = 'https://api.unsplash.com/photos/?client_id=FxDvymrjT20LGA8aaNLkt4N4F8UwviLYDagJT-Ha1_g';
 
-getData(URL).then(function(data){
-    const allBreweries = data;
-    console.log(data);
-    allBreweries.forEach(function(brewery) {
-         const breweryListElement = 
-            `
-            <li> 
-                <img src="${brewery.urls.small}">
-                <p>${brewery.user.name}</p>
-            </li>
-            `
-         list.insertAdjacentHTML('beforeend', breweryListElement)
+// Filter Constructors
+const natureCheckbox = document.querySelector('input#nature');
+const natureImages = document.querySelectorAll('figure.nature');
+const buildingsCheckbox = document.querySelector('input#buildings');
+const buildingsImages = document.querySelectorAll('figure.buildings');
+const carsCheckbox = document.querySelector('input#cars');
+const carsImages = document.querySelectorAll('figure.cars');
+const animalsCheckbox = document.querySelector('input#animals');
+const animalsImages = document.querySelectorAll('figure.animals');
+
+// Like Constructors
+const allImages = document.querySelectorAll('figure');
+
+
+// Filter function
+natureImages.forEach(image => {
+    natureCheckbox.addEventListener('click', (event) => {
+        image.classList.toggle('show');
     });
 });
 
-async function getData(url){
-    const result = await fetch(url)
-    const data = await result.json()
-    return data;
-};
+buildingsImages.forEach(image => {
+    buildingsCheckbox.addEventListener('click', (event) => {
+        image.classList.toggle('show');
+    });
+});
+
+carsImages.forEach(image => {
+    carsCheckbox.addEventListener('click', (event) => {
+        image.classList.toggle('show');
+    });
+});
+
+animalsImages.forEach(image => {
+    animalsCheckbox.addEventListener('click', (event) => {
+        image.classList.toggle('show');
+    });
+});
+
+// Like function
+allImages.forEach(image => {
+    image.addEventListener('dblclick', (event) => {
+        image.classList.toggle('liked');
+    });
+});
