d6324f1c6da4eb7c443f4767ac85bc6b953625d4
Ravi Stil <ravi.raaf@gmail.com>
2021-06-09 23:40:05 +0200

    Update (Not done yet)

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,46 +1,68 @@
-// Filter Constanten
-const natureCheckbox = document.querySelector('input#nature');
-const natureImages = document.querySelectorAll('figure.nature');
-const buildingsCheckbox = document.querySelector('input#buildings');
-const buildingsImages = document.querySelectorAll('figure.buildings');
-const carsCheckbox = document.querySelector('input#cars');
-const carsImages = document.querySelectorAll('figure.cars');
-const animalsCheckbox = document.querySelector('input#animals');
-const animalsImages = document.querySelectorAll('figure.animals');
+/* Filter Constants */
+var optionAll = document.querySelector("#filter-all");
+var optionAnimals = document.querySelector("#filter-animals");
+var optionBuildings = document.querySelector("#filter-buildings");
+var optionNature = document.querySelector("#filter-nature");
+var optionCars = document.querySelector("#filter-cars");
+
+/* Filter Function */
+function filterList(event){
+    let deLijst = document.querySelector("ul");
+    let nieuweFilter = event.target.value;
+    deLijst.className = "";
+    deLijst.classList.add(nieuweFilter);
+}
+
+/* Filter Eventlisteners */
+optionAll.addEventListener("change", filterList);
+optionAnimals.addEventListener("change", filterList);
+optionBuildings.addEventListener("change", filterList);
+optionNature.addEventListener("change", filterList);
+optionCars.addEventListener("change", filterList);
+
+
+
+/* Search */
+var options = {
+    valueNames: [ 'name', 'tags' ]
+};
+
+var charactersList = new List('theList', options);
+
+
+/* Image Lightbox */
+$(document).ready(function() {
+    $('.image-link').magnificPopup({type:'image'});
+});
+
+$('.popup-link').magnificPopup({
+    type: 'image'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Like Constructors
-const allImages = document.querySelectorAll('figure');
+// const allImages = document.querySelectorAll('figure');
 
-
-// Filter function
-// Dit is een betere manier
-    natureCheckbox.addEventListener('click', (event) => {
-        natureImages.forEach(image => {
-              image.classList.toggle('show');
-    });
-});
-
-buildingsImages.forEach(image => {
-    buildingsCheckbox.addEventListener('click', (event) => {
-        image.classList.toggle('show');
-    });
-});
-
-carsImages.forEach(image => {
-    carsCheckbox.addEventListener('click', (event) => {
-        image.classList.toggle('show');
-    });
-});
-
-animalsImages.forEach(image => {
-    animalsCheckbox.addEventListener('click', (event) => {
-        image.classList.toggle('show');
-    });
-});
-
-// Like function
-allImages.forEach(image => {
-    image.addEventListener('dblclick', (event) => {
-        image.classList.toggle('liked');
-    });
-});
+// // Like function
+// allImages.forEach(image => {
+//     image.addEventListener('dblclick', (event) => {
+//         image.classList.toggle('liked');
+//     });
+// });
